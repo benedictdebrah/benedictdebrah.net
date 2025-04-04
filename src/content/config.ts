42e71115fb0ff1691cc198785dc698ba4de6a5ed
benedictdebrah <benedictdebrah.o@gmail.com>
2025-04-04 09:06:15 +0000

    fix: template to continue

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,12 +15,24 @@ const blogSchema = z.object({
     published: z.boolean(),
 });
 
+const aboutSchema = z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+});
+
 const blogCollection = defineCollection({
     type: "content",
     schema: blogSchema,
 });
 
+const aboutCollection = defineCollection({
+    type: "content",
+    schema: aboutSchema,
+});
+
 export type BlogType = z.infer<typeof blogSchema>;
+export type AboutType = z.infer<typeof aboutSchema>;
 export const collection = {
     blog: blogCollection,
+    about: aboutCollection,
 };
